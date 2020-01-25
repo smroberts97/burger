28793a99fff55f2a2eeadcd696a3c8db8ab86fb4
Shannon Roberts <smrobertsllc@gmail.com>
2020-01-25 15:16:40 -0500

    added config files

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-//Require Express//
+
+var express = require("express");
+var exphbs = require("express-handlebars");
+var routes = require("./controllers/burgers_controller.js")
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Listening on PORT: " + PORT);
+});
